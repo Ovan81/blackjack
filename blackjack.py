36eeed3221109c8e6da7559a387be29d6e6765d6
Ovan81 <114009951+Ovan81@users.noreply.github.com>
2024-01-18 13:54:47 +0100

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,28 +1,35 @@
 import random
 import time
+base=["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "knight", "queen", "king"]
 
-clubs=["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "knight", "queen", "king"]
-hearts=["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "knight", "queen", "king"]
-spades=["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "knight", "queen", "king"]
-diamonds=["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "knight", "queen", "king"]
-
-
+clubs=base
+hearts=base
+spades=base
+diamonds=base
+hand=0
+dealerhand=0
 def cardpicker():
-    
     choice=(random.randint(0, len(clubs) + len(hearts) + len(spades) + len(diamonds)))
     if choice <= len(clubs):
         suit = "clubs"
+        choice = clubs.pop(choice - 1)
     elif choice <= len(clubs) + len(hearts):
         suit = "hearts"
+        choice = hearts.pop(choice - len(clubs) - 1)
     elif choice <= len(clubs) + len(hearts) + len(spades):
         suit = "spades"
+        choice = spades.pop(choice - len(hearts) - len(clubs) - 1)
     else:
-    suit = "diamonds"
-print (cardpicker)
+        suit = "diamonds"
+        choice = diamonds.pop(choice - len(hearts) - len(spades) - len(clubs) - 1)
+    cardlist = [choice, suit]
+    return cardlist
+
+cardlist=cardpicker()
+print(cardlist)
 play=input("do you wish to play blackjack?").lower()
 print(play)
 while play != "true" and play != "false":
-    print(play)
     if play == "yes" or play == "yea!" or play == "perchance":
         play="true"
     elif play == "no":
@@ -31,5 +38,8 @@ while play != "true" and play != "false":
     else:
         print("TROGLODYTE UNGA BUNGA MOMENT")
         play=input("do you wish to play blackjack?").lower()
-        
-
+while play == "true":
+    cardlist=cardpicker()
+    hand=hand + int(cardlist[0])
+    print(hand)
+    
