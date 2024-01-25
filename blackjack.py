c9bc7d8ef443b6547cc1a500b14e92e1f25b3cec
Ovan81 <114009951+Ovan81@users.noreply.github.com>
2024-01-25 13:51:17 +0100

    start av objektorientering!"

--- blackjack.py
+++ blackjack.py
@@ -1,16 +1,35 @@
+#blackjack-regler tas frän https://www.star.com.au/goldcoast/sites/star.com.au.goldcoast/files/Blackjack.pdf
 import random
 import time
-base=["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "knight", "queen", "king"]
 
+
+ace=1
+knight=10
+queen=10
+king=10
+base=[ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, knight, queen, king]
 clubs=base
 hearts=base
 spades=base
 diamonds=base
-hand=0
-dealerhand=0
+
+class dealer:
+    hand=0
+
+
+class player:
+    hand=0
+
+players = []
+
+amount_of_p = int(input("eopfewokf"))
+
+for i in range(amount_of_p):
+    players[i] = player()
+
 def cardpicker():
     choice=(random.randint(0, len(clubs) + len(hearts) + len(spades) + len(diamonds)))
-    if choice <= len(clubs):
+    if choice <= len(clubs) :
         suit = "clubs"
         choice = clubs.pop(choice - 1)
     elif choice <= len(clubs) + len(hearts):
@@ -24,22 +43,31 @@ def cardpicker():
         choice = diamonds.pop(choice - len(hearts) - len(spades) - len(clubs) - 1)
     cardlist = [choice, suit]
     return cardlist
+ 
 
-cardlist=cardpicker()
-print(cardlist)
+
 play=input("do you wish to play blackjack?").lower()
-print(play)
-while play != "true" and play != "false":
+while play != True and play != False:
     if play == "yes" or play == "yea!" or play == "perchance":
-        play="true"
+        play= True
     elif play == "no":
-        play = "false"
+
+        play = False
         print("D:")
     else:
         print("TROGLODYTE UNGA BUNGA MOMENT")
         play=input("do you wish to play blackjack?").lower()
-while play == "true":
-    cardlist=cardpicker()
+while play == True:
+    cardlist = cardpicker()
     hand=hand + int(cardlist[0])
-    print(hand)
+    print(cardlist)
+    print("you got", cardlist[0], "of", cardlist[1])
+    cardlist = cardpicker()
+    dealerhand = dealerhand + int(cardlist[0])
+
+
+
+
+
+    break
     
