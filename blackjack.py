49d0d49c48e8f7b433a30da593553ecfd21baa0e
Ovan81 <114009951+Ovan81@users.noreply.github.com>
2024-03-04 13:31:04 +0100

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -13,6 +13,24 @@ hearts=base
 spades=base
 diamonds=base
 
+class rules:
+    def cardpicker(self):
+        choice=(random.randint(0, len(clubs) + len(hearts) + len(spades) + len(diamonds)))
+        if choice <= len(clubs) :
+            suit = "clubs"
+            choice = clubs.pop(choice - 1)
+        elif choice <= len(clubs) + len(hearts):
+            suit = "hearts"
+            choice = hearts.pop(choice - len(clubs) - 1)
+        elif choice <= len(clubs) + len(hearts) + len(spades):
+            suit = "spades"
+            choice = spades.pop(choice - len(hearts) - len(clubs) - 1)
+        else:
+            suit = "diamonds"
+            choice = diamonds.pop(choice - len(hearts) - len(spades) - len(clubs) - 1)
+        cardlist = [choice, suit]
+        return cardlist
+
 class dealer:
     hand=0
 
@@ -27,45 +45,34 @@ while not playercount.isdigit():
     playercount = input("wrong input, type in numbers only please!")
 playercount = int(playercount)
 for i in range(playercount):
-    players[i] = player()
-print(len.players)
-def cardpicker():
-    choice=(random.randint(0, len(clubs) + len(hearts) + len(spades) + len(diamonds)))
-    if choice <= len(clubs) :
-        suit = "clubs"
-        choice = clubs.pop(choice - 1)
-    elif choice <= len(clubs) + len(hearts):
-        suit = "hearts"
-        choice = hearts.pop(choice - len(clubs) - 1)
-    elif choice <= len(clubs) + len(hearts) + len(spades):
-        suit = "spades"
-        choice = spades.pop(choice - len(hearts) - len(clubs) - 1)
-    else:
-        suit = "diamonds"
-        choice = diamonds.pop(choice - len(hearts) - len(spades) - len(clubs) - 1)
-    cardlist = [choice, suit]
-    return cardlist
- 
+    players.append(player())
+print(len(players))
 
+
+rules=rules()
+dealer= dealer()
 
 play=input("do you wish to play blackjack?").lower()
 while play != True and play != False:
     if play == "yes" or play == "yea!" or play == "perchance":
         play= True
     elif play == "no":
-
-        play = False
-        print("D:")
+         play = False
+         print("D:")
     else:
         print("TROGLODYTE UNGA BUNGA MOMENT")
         play=input("do you wish to play blackjack?").lower()
+        
 while play == True:
-    cardlist = cardpicker()
-    hand=hand + int(cardlist[0])
-    print(cardlist)
-    print("you got", cardlist[0], "of", cardlist[1])
-    cardlist = cardpicker()
-    dealerhand = dealerhand + int(cardlist[0])
+    for i in range (playercount):
+        cardlist = rules.cardpicker()
+        players[i].hand = players[i].hand + cardlist[0]
+        print(cardlist)
+        print("you got", cardlist[0], "of", cardlist[1])
+
+    cardlist = rules.cardpicker()
+    dealer.hand = dealer.hand + rules.cardpicker()
+    print(dealer.hand)
 
 
 
