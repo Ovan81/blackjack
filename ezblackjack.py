4996edb72d744024932063887eb6ac03fca51296
Ovan81 <114009951+Ovan81@users.noreply.github.com>
2024-05-10 16:36:32 +0200

    fixat mycket :b

--- ezblackjack.py
+++ ezblackjack.py
@@ -1,24 +1,94 @@
+#blackjack-regler tas frän https://www.star.com.au/goldcoast/sites/star.com.au.goldcoast/files/Blackjack.pdf, men ändringar i reglerna kan ske.
+import random
+question = "do you want to play? (y/n)"
+playerhand=[]
+computerhand=[]
+deck=[]
 
-
-def play():
+def play(question):
     isplaying = ""
     
     while isplaying !=True and isplaying != False:
-        play=input("do you wish to play blackjack? (y/n)").lower()
+        play=input(question).lower()
         if play == "y":
             isplaying = True
         elif play == "n":
             isplaying = False
             print("too bad :(")
         else:
-            print("Type Y or N please ")
-            play=input("do you wish to play blackjack?").lower()
+            play=input("type y or n, please.").lower()
     return isplaying
 
-while play() == True:
-    deck=[]
+def cardsuit(card):
+    if card <= 13:
+        suit= "clubs"
+    elif card > 13 and card <= 26:
+        suit = "diamonds"
+    elif card > 26 and card <= 39:
+        suit = "hearts"
+    else:
+        suit = "spades"
+
+    return suit
+
+def cardindex (card): 
+    cardvalue=card
+    if cardvalue <= 13:
+        None
+    elif cardvalue >= 13 and cardvalue < 26:
+        cardvalue -= 13
+    elif cardvalue >= 26 and cardvalue < 39:
+        cardvalue -= 26
+    else:
+        cardvalue -= 39
+
+    if cardvalue == 1:
+        cardvalue ="ace"
+    elif cardvalue == 11:
+        cardvalue = "knight"
+    elif cardvalue == 12:
+        cardvalue = "queen"
+    elif cardvalue == 13:
+        cardvalue = "king"
+    else:
+        None
+    return cardvalue
+    
+    
+
+def addcardtohand(deck, hand):
+        tempcard = deck[0]
+        deck.pop(0)
+        hand.append(tempcard)
+
+
+while play(question) == True:
+    deck.clear
     for i in range(1,53):
         deck.append(i)
+    random.shuffle(deck)
         
 
-    break
+
+    addcardtohand(deck, playerhand)
+    cardtype=cardsuit(playerhand[-1])
+    cardvalue=cardindex(playerhand[-1])
+    print("you got", cardvalue, "of", cardtype + "!")
+
+    addcardtohand(deck, computerhand)
+    cardtype=cardsuit(computerhand[-1])
+    cardvalue=cardindex(computerhand[-1])
+    print("The dealer got", cardvalue, "of", cardtype + "!")
+
+    addcardtohand(deck, playerhand)
+    cardtype=cardsuit(playerhand[-1])
+    cardvalue=cardindex(playerhand[-1])
+    print("you got", cardvalue, "of", cardtype + "!")
+    
+
+   
+
+    question = ("do you want to play again? (y/n)")
+    
+
+    
